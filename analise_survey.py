1b336c38e1aaa6da8a0036e890fbb045f544c82e
CaioSoares-V8 <caio.soares@v8.tech>
2024-10-01 12:56:52 -0300

    Adicionando interações com o Streamlit

--- analise_survey.py
+++ analise_survey.py
@@ -1,76 +1,48 @@
-"""
-# Análise Survey Stack Overflow
-
-## Contexto
-
-Com o avanço significativo dos Modelos de Inteligência Artificial LLM e o surgimento de diversas ferramentas que os utilizam, a empresa V8.Tech está interessada em incorporar algumas dessas soluções no dia a dia de seus colaboradores.
-
-Para tomar uma decisão mais assertiva sobre qual ferramenta adotar, foi solicitado que uma análise seja feita utilizando os registros de dados da pesquisa **Survey Stack Overflow 2024**, que em meio a tratativa de diversos assuntos relacionados a tecnologia, investigou o uso de IA entre profissionais de tecnologia e desenvolvimento, abordando questões como popularidade das ferramentas, confiabilidade e utilidade em tarefas mais complexas.
-
-## Extração
-
-Importando as bibliotecas necessárias
-"""
-
 import pandas as pd
-import matplotlib.pyplot as plt
+import streamlit as st
+import numpy as np
 
-dados = pd.read_csv('survey_results_public.csv')
 
-
-"""Visualizando o DataFrame em detalhes"""
-
-dados.head()
-
-dados.columns
-
-dados.size
-
-dados.shape
-
-"""O arquivo bruto contém um total de **7.459.818** dados, organizados em **65.437** linhas e **114** colunas, abrangendo assuntos como: Educação, Trabalho, Carreira, Linguagens, Frameworks, Softwares e Sistemas Operacionais mais utilizados, Comunidade Stack Overflow e Inteligência Artificial.
-
-Diante da necessidade de uma análise mais precisa e direcionada, se torna importante filtrar as informações mais relevantes para o nosso contexto.
-
-## Tratamento
-
-Selecionando as colunas relevantes
-"""
+url = 'https://raw.githubusercontent.com/CaioSoares-V8/Analise_Survey_Stack_Overflow/refs/heads/main/survey_data.csv'
+dados = pd.read_csv(url)
 
 dados_tratados = dados[['ResponseId', 'MainBranch', 'Age', 'Employment',
                         'YearsCodePro', 'DevType', 'Country','AISearchDevHaveWorkedWith',
                         'AISearchDevWantToWorkWith', 'AISearchDevAdmired', 'AISelect', 'AISent',
                         'AIBen', 'AIAcc', 'AIComplex', 'AIThreat', 'ProfessionalQuestion', 'Industry', 'JobSat']]
 
-"""
-Visualizando os dados selecionados
-"""
-
-dados_tratados.head()
-
-"""Removendo os dados nulos referentes a utilização de IA"""
-
 dados_tratados = dados_tratados.dropna(subset=['AISearchDevHaveWorkedWith', 'AISearchDevWantToWorkWith', 'AISearchDevAdmired'])
 
-"""Agrupando a sessão de respostas referentes a utilização de IA"""
-
 AI_search = dados_tratados[['AISearchDevHaveWorkedWith', 'AISearchDevWantToWorkWith', 'AISearchDevAdmired']]
-
-"""Fragmentando a coluna, possibilitando a análise isolada e o agrupamento de cada IA específica"""
 
 have_worked_ia = AI_search['AISearchDevHaveWorkedWith'].str.get_dummies(';')
 want_to_work_ia = AI_search['AISearchDevWantToWorkWith'].str.get_dummies(';')
 admired_ia = AI_search['AISearchDevAdmired'].str.get_dummies(';')
 
-"""Visualizando as colunas"""
-
-have_worked_ia.head()
-
-"""## Compactando o DataFrame
-
-Unindo os dados tratados ao DataFrame de utilização de IAs
-"""
-
 dados = pd.concat([dados_tratados, have_worked_ia], axis=1)
 
-dados.to_csv('survey_data.csv', index=False)
+st.write('Dados tratados do Survey Stack Overflow 2024')
+
+st.dataframe(dados.head())
+
+resultado_paises = dados.groupby('Country')['ChatGPT'].sum()
+
+have_worked_ia_results = {}
+
+for i in have_worked_ia.columns:
+    soma = have_worked_ia[i].sum()
+    have_worked_ia_results[i] = soma
+
+total_worked = sum(have_worked_ia_results.values())
+have_worked_ia_results = {chave: round((valor / total_worked) * 100, 1) for chave, valor in have_worked_ia_results.items()}
+
+sum(have_worked_ia_results.values())
+have_worked_ia_results = dict(sorted(have_worked_ia_results.items(), key=lambda x: x[1], reverse=True)[:5])
+
+st.write('Top 5 - IAs mais utilizadas em 2023')
+
+if st.button('Mostrar gráfico'):
+    st.bar_chart(have_worked_ia_results)
+
+
+
